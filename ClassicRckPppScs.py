ab40fa14f8d31ae4aae2d0ec5ad1d2b30ffee550
Igna-M <ign.manzo@gmail.com>
2021-04-10 12:07:15 -0300

    Classic game

--- ClassicRckPppScs.py
+++ ClassicRckPppScs.py
@@ -1,12 +1,12 @@
 import random
 
 def strike():
-    hand = random.choice(['Stone', 'Paper', 'Scissor'])
+    hand = random.choice(['Rock', 'Paper', 'Scissor'])
     return hand
 
 def convert_int(num):
     if num == '1':
-        weapon = 'Stone'
+        weapon = 'Rock'
     elif num == '2':
         weapon = 'Paper'
     else:
@@ -14,10 +14,10 @@ def convert_int(num):
     return weapon
 
 def who_won(human_weapon, robot):
-    if (human_weapon == 'Stone' and robot == 'Stone') or (human_weapon == 'Paper' and robot == 'Paper') or (human_weapon == 'Scissor' and robot == 'Scissor'):
+    if (human_weapon == 'Rock' and robot == 'Rock') or (human_weapon == 'Paper' and robot == 'Paper') or (human_weapon == 'Scissor' and robot == 'Scissor'):
         winner = 'none'
         print('Empate')
-    elif (human_weapon == 'Stone' and robot == 'Scissor') or (human_weapon == 'Paper' and robot == 'Stone') or (human_weapon == 'Scissor' and robot == 'Paper'):
+    elif (human_weapon == 'Rock' and robot == 'Scissor') or (human_weapon == 'Paper' and robot == 'Rock') or (human_weapon == 'Scissor' and robot == 'Paper'):
         winner = 'human'
         print('Ganó el humano')
     else:
@@ -30,7 +30,7 @@ def query_player():
     options = ['1', '2', '3']
     human = 0
     while human not in options:
-        human = input('Stone(1), Paper(2), or Scissor(3)? ')
+        human = input('Rock(1), Paper(2), or Scissor(3)? ')
         if human not in options:
             print('You have to type a number between 1 and 3 (1, 2 or 3).')
             print('')
